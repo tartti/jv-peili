b13947204005ec4b955780d34b2f6c89fff561ad
tartti <tarthix@gmail.com>
2017-03-26 04:32:47 +0300

    Perus lämpötilan saanti ja tulostusta hoituu

--- peili_gui.py
+++ peili_gui.py
@@ -1,3 +1,8 @@
 import saaPohja
+from tkinter import *
 
-print(saaPohja.anna_lampo())
+root = Tk()
+theLabel = Label(root, text="lämpotila jyväskylässä on %s C"%(saaPohja.anna_lampo()))
+theLabel.pack()
+root.mainloop()
+#print(saaPohja.anna_lampo())
