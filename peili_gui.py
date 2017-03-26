ff07350b69cea13c7d5242d2053a6cd7007c2416
tartti <tarthix@gmail.com>
2017-03-26 16:53:01 +0300

    gui muutoksia

--- peili_gui.py
+++ peili_gui.py
@@ -1,8 +1,15 @@
 import saaPohja
 from tkinter import *
 
+
+def onKeyPress(event, self):
+    self.root.destroy()
+
 root = Tk()
+root.attributes('-fullscreen', True)
 theLabel = Label(root, text="lämpotila jyväskylässä on %s C"%(saaPohja.anna_lampo()))
 theLabel.pack()
+root.bind('<KeyPress>', onKeyPress())
+
 root.mainloop()
 #print(saaPohja.anna_lampo())
