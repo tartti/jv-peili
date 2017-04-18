cdc5427a311b778cf8a1666feeb5262937768b5c
tartti <tarthix@gmail.com>
2017-04-18 14:38:31 +0300

    gui muutoksia

--- peili_gui.py
+++ peili_gui.py
@@ -1,19 +1,31 @@
 import saaPohja
+import time
 from tkinter import *
 
 
-#def onKeyPress(event):
-#    root.destroy()
+def luoNakyma():
+    global lampoTeksti
+    lampoTeksti = Label(root, text="lämpotila jyväskylässä on %s C"%(saaPohja.anna_lampo()))
+    lampoTeksti.config(font=("Courier", 44))
+    lampoTeksti.config(fg = 'white', bg = 'black')
+    lampoTeksti.pack()
+
+
+def paivitaSaa():
+    global lampoTeksti
+    lampoTeksti.config(text = saaPohja.anna_lampo())
+    root.after(2000, paivitaSaa)
+
 
 
 root = Tk()
 root.configure(background='black')
 root.attributes('-fullscreen', True)
-lampoTeksti = Label(root, text="lämpotila jyväskylässä on %s C"%(saaPohja.anna_lampo()))
-lampoTeksti.config(font=("Courier", 44))
-lampoTeksti.config(fg = 'white', bg = 'black')
-lampoTeksti.pack()
+luoNakyma()
+paivitaSaa()
 root.bind("<Escape>", lambda e: root.destroy())
-
 root.mainloop()
+
+
+
 #print(saaPohja.anna_lampo())
