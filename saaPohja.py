638adb3b52c687e2d017e75dae21182d200dd9bc
tartti <tarthix@gmail.com>
2017-04-18 17:19:55 +0300

    testailua

--- saaPohja.py
+++ saaPohja.py
@@ -2,7 +2,9 @@ import json
 from urllib.request import urlopen
 
 
-def saaTila():
+# vaihtoehdot: clear sky; few clouds; scattered clouds; broken clouds;
+#              shower rain; rain; thunderstorm; snow; mist
+def saa_tila():
 
     print("ja det gör jag")
 
@@ -11,16 +13,16 @@ def anna_lampo():
     # url = "tähänurl"
     # response = urlopen(url)
     # data = json.loads(response.read())
-    # ylläoleva on toimiva reaaliaikaisen haun lämpötilan suhteen, mutta testiä varten käytetään alempaa jottei kysytä liian usein säätä
+    # ylläoleva on toimiva reaaliaikaisen haun lämpötilan suhteen, mutta testiä varten käytetään alempaa
+    # jottei kysytä liian usein säätä
     saa = json.dumps({"coord":{"lon":25.75,"lat":62.25},"weather":[{"id":800,"main":"Clear","description":"clear sky","icon":"01n"}],"base":"stations","main":{"temp":269.15,"pressure":1014,"humidity":100,"temp_min":269.15,"temp_max":269.15},"visibility":10000,"wind":{"speed":2.82,"deg":238.003},"clouds":{"all":0},"dt":1490482200,"sys":{"type":1,"id":5025,"message":0.0095,"country":"FI","sunrise":1490414304,"sunset":1490460506},"id":655195,"name":"Jyväskylä","cod":200})
     data = json.loads(saa)
 
     tila = data['weather'][0]['description']
     print(tila)
     lampo = data['main']['temp'] - 273.15
-    #print("Jyväskylän lämpötila on nyt %s C"%(lampo))
+    # print("Jyväskylän lämpötila on nyt %s C"%(lampo))
     return lampo
-
 
 
 print(anna_lampo())
